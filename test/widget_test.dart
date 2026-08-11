b26c35223554f6bfaceea6560646e3ff79b235a0
Aide Ortiz <lizaide04@gmail.com>
2026-08-11 11:55:14 -0600

    Agregar prueba de calculo del carrito

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,11 +1,28 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('Prueba básica de TacoExpress', () {
-    int cantidad = 1;
+  test('Calcula correctamente el total del carrito', () {
+    final productos = [
+      {
+        'nombre': 'Taco de Asada',
+        'precio': 25.0,
+        'cantidad': 2,
+      },
+      {
+        'nombre': 'Taco de Pastor',
+        'precio': 22.0,
+        'cantidad': 1,
+      },
+    ];
 
-    cantidad++;
+    double total = 0;
 
-    expect(cantidad, 2);
+    for (var producto in productos) {
+      total +=
+          (producto['precio'] as double) *
+          (producto['cantidad'] as int);
+    }
+
+    expect(total, 72.0);
   });
 }
