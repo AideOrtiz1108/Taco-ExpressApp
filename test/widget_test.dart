6c413283f02a1c75d9f9407b044439d4bcdc9dee
Aide Ortiz <lizaide04@gmail.com>
2026-08-11 11:46:23 -0600

    Agregar prueba básica de TacoExpress

---
+++ test/widget_test.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Prueba básica de TacoExpress', () {
+    int cantidad = 1;
+
+    cantidad++;
+
+    expect(cantidad, 2);
+  });
+}
